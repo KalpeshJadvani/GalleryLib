0a2155770aeaffd815088a6d598fbf0c4d99a40a
jadvani75 <jadvani75@gmail.com>
2018-10-23 03:37:04 +0530

    update api 28

--- app/src/main/java/com/example/kalpesh/gallery/CustomGrid.java
+++ app/src/main/java/com/example/kalpesh/gallery/CustomGrid.java
@@ -6,12 +6,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 
 import com.bumptech.glide.Glide;
-import com.squareup.picasso.MemoryPolicy;
-import com.squareup.picasso.NetworkPolicy;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
